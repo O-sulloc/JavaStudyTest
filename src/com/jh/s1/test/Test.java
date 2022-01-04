d345d9bcf5bb96e564d78df62eb36747c38e04a6
O-sulloc <bujjaf@gmail.com>
2022-01-04 12:14:37 +0900

    add why

--- src/com/jh/s1/test/Test.java
+++ src/com/jh/s1/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Why I can't delete my workspace projects!?");
+		System.out.println("뭐냐...? 왜 깃허브 연동 풀렸냐 왜");
 	}
 
 }
